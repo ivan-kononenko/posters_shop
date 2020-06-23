518f38aa7b712f07ed03b26076bd27e1e16418fe
HOME\PrinceOfPersik <ivan.a.kononenko@gmail.com>
2020-06-23 19:42:01 +0300

    addition of paths

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('catalog/<int:catalogl1_id>/', views.catalog, name='catalog'),
+    path('catalog/<int:catalogl1_id>/<int:catalogl2_id>/', views.catalog, name='catalog'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import CategoryL1, CategoryL2
+
 x = 10
 x1 = x + x
 y = 10
@@ -11,6 +13,15 @@ if x1 is y1:
     variable_a = True
 else:
     variable_a = False
+
+
 def index(request):
     return HttpResponse(variable_a)
 
+
+def catalog(request, catalogl1_id, catalogl2_id=None):
+    if catalogl2_id:
+        cat = CategoryL2.objects.get(pk=catalogl2_id)
+    else:
+        cat = CategoryL1.objects.get(pk=catalogl1_id)
+    return HttpResponse(f"{catalogl1_id} - {catalogl2_id}<br>{cat.name}")
