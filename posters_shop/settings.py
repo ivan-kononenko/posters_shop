e5081428de180bb055c6aa3de30a39b21bdec98e
HOME\PrinceOfPersik <ivan.a.kononenko@gmail.com>
2020-07-20 11:03:53 +0300

    Change of all .html files to work with base template extending.

--- posters_shop/settings.py
+++ posters_shop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'posters_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
